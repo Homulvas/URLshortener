3038e247b9f79cd3d4795da8ba70669d888a6f66
Ignas Kaziukėnas <ignas@ignas.co>
2017-04-10 21:55:28 +0300

    validate input url and route correctly

--- server.js
+++ server.js
@@ -3,33 +3,45 @@ var app = express()
 var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/test2';
 var mongodb = require("mongodb");
 var mongo = mongodb.MongoClient
+    var validUrl = require('valid-url');
 
 app.get('/add/*', function (req, res) {
     var url = req.params[0]
 
-    mongo.connect(mongoUrl, function (err, db) {
-        if (err) {
-            return console.log(err);
-        }
-        var coll = db.collection('test2');
-        var testObj = { test: 'aa', bb: 'cc' }
-        coll.insert(testObj, function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
+    if (validUrl.isUri(url)) {
+        res.send('valid')
+    } else {
+        res.send('y tho')
+    }
 
-            console.log(JSON.stringify(data));
-        })
-        coll.count({}, function (err, data) {
-            if (err) {
-                return console.log(err);
-            }
-            res.send({ resp: data })
-        })
+    // mongo.connect(mongoUrl, function (err, db) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     var coll = db.collection('test');
+    //     var testObj = { test: 'aa', bb: 'cc' }
+    //     coll.insert(testObj, function (err, data) {
+    //         if (err) {
+    //             return console.log(err);
+    //         }
 
-        db.close();
-    })
+    //         console.log(JSON.stringify(data));
+    //     })
+    //     coll.count({}, function (err, data) {
+    //         if (err) {
+    //             return console.log(err);
+    //         }
+    //         res.send({ resp: data })
+    //     })
 
+    //     db.close();
+    // })
+
+})
+
+app.get('/:id(\\d+)/', function (req, res) {
+    
+    res.send(req.params.id)
 })
 
 var port = process.env.PORT || 8080
