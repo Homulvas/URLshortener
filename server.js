027bd60ed9688c8f6aacfc5825b1224fdd0d6eee
Ignas Kaziukėnas <ignas@ignas.co>
2017-04-11 22:53:05 +0300

    use mongo unique id for shortening

--- server.js
+++ server.js
@@ -3,45 +3,49 @@ var app = express()
 var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/test2';
 var mongodb = require("mongodb");
 var mongo = mongodb.MongoClient
-    var validUrl = require('valid-url');
+var validUrl = require('valid-url');
 
 app.get('/add/*', function (req, res) {
     var url = req.params[0]
 
     if (validUrl.isUri(url)) {
-        res.send('valid')
+        mongo.connect(mongoUrl, function (err, db) {
+            if (err) {
+                return res.send(err);
+            }
+            var coll = db.collection('urls');
+            var urlObj = { url: url }
+            coll.insert(urlObj, function (err, data) {
+                if (err) {
+                    return res.send(err);
+                }
+            })
+            var response = { url: req.headers.host + '/' + urlObj._id }
+            res.send(JSON.stringify(response))
+
+            db.close();
+        })
     } else {
-        res.send('y tho')
+        var error = { error: 'Url seems to be invalid' }
+        res.send(JSON.stringify(error))
     }
-
-    // mongo.connect(mongoUrl, function (err, db) {
-    //     if (err) {
-    //         return console.log(err);
-    //     }
-    //     var coll = db.collection('test');
-    //     var testObj = { test: 'aa', bb: 'cc' }
-    //     coll.insert(testObj, function (err, data) {
-    //         if (err) {
-    //             return console.log(err);
-    //         }
-
-    //         console.log(JSON.stringify(data));
-    //     })
-    //     coll.count({}, function (err, data) {
-    //         if (err) {
-    //             return console.log(err);
-    //         }
-    //         res.send({ resp: data })
-    //     })
-
-    //     db.close();
-    // })
-
 })
 
-app.get('/:id(\\d+)/', function (req, res) {
-    
-    res.send(req.params.id)
+app.get('/:id(\\w+)/', function (req, res) {
+    mongo.connect(mongoUrl, function (err, db) {
+        if (err) {
+            return res.send(err);
+        }
+        var coll = db.collection('urls');
+        var urlObj = coll.findOne(mongodb.ObjectId(req.params.id), function (err, urlObj) {
+            if (err) {
+                return res.send(err);
+            }
+            res.redirect(urlObj.url)
+        })
+
+        db.close();
+    })
 })
 
 var port = process.env.PORT || 8080
